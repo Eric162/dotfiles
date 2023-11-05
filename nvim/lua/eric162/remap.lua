e768b51c76059c2ee21f8bc81ce4e18f51e9a858
Eric Shirley <eric.s.shirley@gmail.com>
2023-11-05 18:27:31 -0500

    some additional mappings for moving lines

--- nvim/lua/eric162/remap.lua
+++ nvim/lua/eric162/remap.lua
@@ -8,3 +8,12 @@ vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 vim.keymap.set("n", "}", "}zz")
 vim.keymap.set("n", "{", "{zz")
+
+-- these should allow moving entire lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- format a file
+vim.keymap.set("x", "<leader>p", [["_dP_dP]])
+
+vim.keymap.set("n", "Q", "<nop>")
