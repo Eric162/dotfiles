d777b13b02f266c4156519d99be7a8aff2af3bbc
Eric Shirley <eric.s.shirley@gmail.com>
2024-01-19 13:26:50 -0500

    nvim surround

--- nvim/init.lua
+++ nvim/init.lua
@@ -124,6 +124,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
   {
